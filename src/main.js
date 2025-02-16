95501300905e70a8ee3a49af9862f1e4f4274616
binghan <binghan60@gmail.com>
2025-02-17 00:43:55 +0800

    佈署wss

--- src/main.js
+++ src/main.js
@@ -19,7 +19,7 @@ defineRule('atLeastOneFieldRule', (value, [field1, field2, field3, field4]) => {
 app.use(createPinia())
 app.use(router)
 app.use(VueVirtualScroller)
-app.use(VueNativeSock, 'ws:' + import.meta.env.VITE_WS_PATH, {
+app.use(VueNativeSock, 'wss:' + import.meta.env.VITE_WS_PATH, {
   reconnection: true, // (Boolean) whether to reconnect automatically (false)
   reconnectionAttempts: 5, // (Number) number of reconnection attempts before giving up (Infinity),
   reconnectionDelay: 3000, // (Number) how long to initially wait before attempting a new (1000)
